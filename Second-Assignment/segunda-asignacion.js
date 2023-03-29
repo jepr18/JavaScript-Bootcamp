09c8a16635b6a21178298a88d68b3b5e46177386
jepr18 <126118906+jepr18@users.noreply.github.com>
2023-03-28 21:01:37 -0400

    Add files via upload

---
+++ Second-Assignment/segunda-asignacion.js
@@ -0,0 +1,11 @@
+// Segunda Asignación
+
+const nombre = "Juan Emmanuel Parra";
+const edad = 23;
+const isDeveloper = false;
+const fecha_nacimiento = new Date(2000, 0, 18);
+
+console.log(nombre);
+console.log(edad);
+console.log(isDeveloper);
+console.log(fecha_nacimiento);
